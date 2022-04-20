8be6b6e7ea2acfc5ed8893b4831b30892fa2ccf3
Vincent <vincent.burkhardt.home@gmail.com>
2022-04-20 19:19:20 +0200

    Update fetch_and_cut.py

--- scripts/fetch_and_cut.py
+++ scripts/fetch_and_cut.py
@@ -4,23 +4,19 @@ import spotipy
 import sys
 
 # things to configure: song path and ffmpeg path / Spotify Playlist ID
-song = AudioSegment.from_file("C:\\Users\\vince\\Desktop\\pydub\\song.wav")
+song = AudioSegment.from_file("C:\\Users\\vince\\Desktop\\pydub\\song.mp3")
 AudioSegment.converter = "C:\\ffmpeg\\ffmpeg\\bin\\ffmpeg.exe"
 AudioSegment.ffmpeg = "C:\\ffmpeg\\ffmpeg\\bin\\ffmpeg.exe"
 AudioSegment.ffprobe ="C:\\ffmpeg\\ffmpeg\\bin\\ffprobe.exe"
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-# get duration of 1 song in ms
-if len(sys.argv) > 1:
-    urn = sys.argv[1]
-else:
-    urn = 'spotify:track:'
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
-song_data = sp.track(urn)
-song_duration_spotify = song_data["duration_ms"]
+# liste mit song id's / songlänge
+songlist = []
+songlength = []
 
-# get songs in playlist
 pl_id = 'spotify:playlist:45OOE0Q5yV9u3uRiSEFdpe'
 offset = 0
 
@@ -32,16 +28,35 @@ while True:
     
     if len(response['items']) == 0:
         break
-    
     offset = offset + len(response['items'])
-    #pprint(response['items'])
-    print(offset, "/", response['total'])
     print()
-    for key in response['items']:
-         print(key)
+    for lists in response['items']:
+         for elements in lists.items():
+             for subelements in elements:
+                 if isinstance(subelements, str):
+                     continue
+                 for subsubelements in subelements.items():
+                     for x in subsubelements[1::2]:
+                         songlist.append(x)
 
-# cut 1 song
-song_duration = len(song) - (len(song)-song_duration_spotify)
-output = song[:song_duration]
+    print(offset, "/", response['total'])
 
-output.export("C:/Users/vince/Desktop/pydub/output.mp3", format="mp3")
+for elements in songlist:
+    print(elements)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# whyyyyyyyyyyyyyyyyyyyyyyy uwu
+# get duration of 1 song in ms
+counter = 0
+for songs in songlist:
+    if len(sys.argv) > 1:
+        urn = sys.argv[1]
+    else:
+        counter +=1
+        urn = 'spotify:track:' + songs
+        song_data = sp.track(urn)
+        song_duration_spotify = song_data["duration_ms"]
+        print("Song geht " + str(song_duration_spotify) + " ms.")
+        song_duration = len(song) - (len(song) - song_duration_spotify)
+        output = song[:song_duration]
+        output.export("C:/Users/vince/Desktop/pydub/output" + str(counter) + ".mp3", format="mp3")
