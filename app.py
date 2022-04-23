73c48949798809ca9d34292c37b3dc8b7df794da
Vincent <vincent.burkhardt.home@gmail.com>
2022-04-23 14:11:08 +0200

    yeah update
    not yet working

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 playlist_songlist = []
 songlength = []
 playlist_id = 'spotify:playlist:45OOE0Q5yV9u3uRiSEFdpe'
-# paths (configurable)
 song = AudioSegment.from_file("C:\\Users\\vince\\Desktop\\Slicetify\\song.mp3")
 AudioSegment.converter = "C:\\ffmpeg\\ffmpeg\\bin\\ffmpeg.exe"
 AudioSegment.ffmpeg = "C:\\ffmpeg\\ffmpeg\\bin\\ffmpeg.exe"
@@ -41,12 +40,16 @@ def getSongList(list, pl_id):
 # cuts and names songs based on the list of songs it gets
 # one argument (list of songs from (getSonglist()))
 def cutAndNameSongs(list):
+    # variables
     counter = 0
     songfilelength = len(song)
     songstarttime = 0
+
+    # iterates given list
     for songs in list:
         if len(sys.argv) > 1:
             urn = sys.argv[1]
+        
         else:
             urn = 'spotify:track:' + songs
             counter +=1
@@ -54,9 +57,12 @@ def cutAndNameSongs(list):
             song_duration_spotify = song_data["duration_ms"]
             sec = round((song_duration_spotify/1000) % 60)
             min = int(song_duration_spotify/1000/60)
+
             print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
             print("fetching song: " + str(song_data["name"]) + "\n" + "duration: "
             + str(min) + " Minuten " + str(sec) + " Sekunden" +"\n" + "converting... " )
+            print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+            
             song_duration = len(song) - (songfilelength - song_duration_spotify)
             output = song[songstarttime:song_duration]
             output.export("C:/Users/vince/Desktop/Slicetify/" + str(song_data["name"]) + ".mp3", format="mp3")
--- gui.py
+++ gui.py
@@ -1,6 +1,15 @@
+from cgitb import enable
+from faulthandler import disable
+from tkinter import DISABLED
 import PySimpleGUI as sg
+import app as app
 
-# work in progress, funktionalität und konfigurationsmöglichkeit muss noch ergänzt werden
+# file and folder information
+audio_path, ffmpeg_path, output_path = '','',''
+# spotify information
+sp_pl, sp_id, sp_se = '','',''
+
+# default configuration of every UI element #
 sg.set_options(
     font="FiraCode 12", 
     background_color="#24292E", 
@@ -18,7 +27,7 @@ layout = [
         background_color = '#24292E',
         key = 'Titlebar',)],
 
-    # AUDIO FILE BROWSER #
+    # HeadLine #
     [
         sg.Text(
             text="Folder Setup", 
@@ -39,28 +48,28 @@ layout = [
     [ 
         sg.FileBrowse(
             button_text ='audio file',
-            key = 'audio_browser',
+            key = '-AUDIO_BROWSER-',
             size=(10,1),
             file_types = (('MP3 Files', '*.mp3'),)),
-        sg.InputText(size=(30, 1), key='audio_input')
+        sg.InputText(size=(30, 1), key='-AUDIO_INPUT-')
     ],
 
     # FFMPEG FILE BROWSER #
     [
         sg.FolderBrowse(
             button_text ='ffmpeg folder',
-            key = 'ffmpeg_browser',
+            key = '-FFMPEG_BROWSER-',
             size=(10,1)),
-        sg.InputText(size=(30, 1), key='ffmpeg_input')
+        sg.InputText(size=(30, 1), key='-FFMPEG_INPUT-')
     ],
 
     # OUTPUT FILE BROWSER #
     [
         sg.FolderBrowse(
             button_text ='output folder',
-            key = 'output_browser',
+            key = '-OUTPUT_BROWSER-',
             size=(10,1)),
-        sg.InputText(size=(30, 1), key='output_input')
+        sg.InputText(size=(30, 1), key='-OUTPUT_INPUT-')
     ],
     
     # Spotify Playlist/ID/SECRET setup #
@@ -68,42 +77,51 @@ layout = [
     # SPOTIFY PLAYLIST_ID #
     [
         sg.Button(
+        key='-PLAYLIST_ID_SAVE-',
         button_text='playlist_id', 
         size=(10,1)),
         sg.Input(
+        key='-PLAYLIST_ID-',
         size=(30,2))
     ],
     # SPOTIFY CLIENT_ID #
     [
         sg.Button(
+        key='-CLIENT_ID_SAVE-',
         button_text='client_id',
         size=(10,1)),
         sg.Input(
+        key='-CLIENT_ID-',
         size=(30,2))
     ],
     # SPOTIFY CLIENT_SECRET #
     [
         sg.Button(
+        key='-CLIENT_SECRET_SAVE-',
         button_text='client_secret',
         size=(10,1)), 
         sg.Input(
+        key='-CLIENT_SECRET-',
         size=(30,2))
     ],
     
     # SLICE AND PROGRESSBAR #
     [sg.Button(
-        button_text='Slice', 
+        button_text='SAVE', 
+        font='FiraCode 14 bold',
+        button_color='#1ED760',
+        key='-SAVE-',
+        size=(31, 1))
+    ],
+    [sg.Button(
+        button_text='SLICE', 
+        disabled=True,
         font='FiraCode 20 bold',
         button_color='#1ED760',
-        key='slice',
+        key='-SLICE-',
         size=(22, 1))
-    ],
-    [sg.ProgressBar(
-        max_value=100, 
-        key='progressbar',
-        orientation='h', 
-        size=(29.5, 40),
-        visible=False)]
+    ]
+    #[sg.ProgressBar(max_value=100, key='progressbar',orientation='h', size=(29.5, 40), visible=False)]
 ]
 
 # display window #
@@ -116,8 +134,23 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
-    if event == 'slice':
-        window['progressbar'].update(visible=True)
+    
+    # save input to variables for further processing (no check yet if valid!!!)
+    if event == '-SAVE-': 
+        # paths
+        audio_path = values['-AUDIO_INPUT-']
+        ffmpeg_path = values['-FFMPEG_INPUT-']
+        output_path = values['-OUTPUT_INPUT-']
+        # spotify
+        sp_pl = str(values['-PLAYLIST_ID-'])
+        sp_id = str(values['-CLIENT_ID-'])
+        sp_se = str(values['-CLIENT_SECRET-'])
+
+        #enable slice button
+        window['-SLICE-'].update(disabled=False)
+        #window['progressbar'].update(visible=True)
+    if event == '-SLICE-':
+        print()
 
 # close window #
 window.close()
